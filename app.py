60ac52242aca59645a24d480c3a9a0f191277b0b
betshug <103988135+betshug@users.noreply.github.com>
2024-10-13 23:00:51 +0200

    test

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('homepage.html')
 
+
+@app.route('/portfolio')
+def portfolio():
+    return "sucuni"
+
 if __name__ == "__main__":
     app.run(debug=True)
